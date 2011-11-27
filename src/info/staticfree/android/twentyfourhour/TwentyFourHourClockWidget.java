585dba17c9a8b7def3045e2a3f0f4d9611faaea1
Steve Pomeroy <steve@staticfree.info>
2011-11-27 16:48:18 -0500

    Caches widget to try and reduce GC churn

--- src/info/staticfree/android/twentyfourhour/TwentyFourHourClockWidget.java
+++ src/info/staticfree/android/twentyfourhour/TwentyFourHourClockWidget.java
@@ -1,17 +1,17 @@
 package info.staticfree.android.twentyfourhour;
 /*
  * Copyright (C) 2011 Steve Pomeroy <steve@staticfree.info>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -26,11 +26,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
-import android.graphics.Canvas;
 import android.widget.RemoteViews;
 
 public class TwentyFourHourClockWidget extends AppWidgetProvider {
+
+	private Analog24HClock clock;
+	private int mWidgetSize;
 
 	/**
 	 * Sending this broadcast intent will cause the clock widgets to update.
@@ -42,6 +43,7 @@ public class TwentyFourHourClockWidget extends AppWidgetProvider {
 		super.onEnabled(context);
 
 		startTicking(context);
+		clock = new Analog24HClock(context);
 	}
 
 	@Override
@@ -76,27 +78,26 @@ public class TwentyFourHourClockWidget extends AppWidgetProvider {
 			int[] appWidgetIds) {
 		final RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget);
 
-		final Analog24HClock clock = new Analog24HClock(context);
-		clock.setShowSeconds(false);
+		if (clock == null){
+			clock = new Analog24HClock(context);
+			clock.setShowSeconds(false);
+			clock.addDialOverlay(new SunPositionOverlay(context));
 
-		clock.addDialOverlay(new SunPositionOverlay(context));
+			final int s = clock.getSuggestedMinimumHeight();
+			clock.measure(s, s);
+			clock.layout(0, 0, s, s);
+			clock.setDrawingCacheEnabled(true);
+			mWidgetSize = s;
 
-		final int s = clock.getSuggestedMinimumHeight();
-		clock.measure(s, s);
-		clock.layout(0, 0, s, s);
+			final PendingIntent intent = ClockUtil.getClockIntent(context);
+			if (intent != null){
+				rv.setOnClickPendingIntent(R.id.clock, intent);
+			}
+		}
 
-		final Bitmap bmp = Bitmap.createBitmap(s, s, Config.ARGB_8888);
-		final Canvas c = new Canvas(bmp);
-		clock.draw(c);
-
-		final Bitmap immutable = Bitmap.createBitmap(bmp);
-		bmp.recycle();
-
-		rv.setImageViewBitmap(R.id.clock, immutable);
-
-		final PendingIntent intent = ClockUtil.getClockIntent(context);
-		if (intent != null){
-			rv.setOnClickPendingIntent(R.id.clock, intent);
+		final Bitmap cached = clock.getDrawingCache(true);
+		if (cached != null){
+			rv.setImageViewBitmap(R.id.clock, cached);
 		}
 
 		appWidgetManager.updateAppWidget(appWidgetIds, rv);

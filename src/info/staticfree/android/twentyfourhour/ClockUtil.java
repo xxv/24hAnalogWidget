54564b8d1978e62b5188b0c1a931eae47f625875
Steve Pomeroy <steve@staticfree.info>
2011-11-27 15:00:58 -0500

    Fixes clock launcher

--- src/info/staticfree/android/twentyfourhour/ClockUtil.java
+++ src/info/staticfree/android/twentyfourhour/ClockUtil.java
@@ -4,6 +4,8 @@ import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 
 public class ClockUtil {
 
@@ -13,7 +15,8 @@ public class ClockUtil {
 	 * @see <a href="http://stackoverflow.com/questions/3590955/intent-to-launch-the-clock-application-on-android">Stack Overflow post</a>
 	 */
 	public static final PendingIntent getClockIntent(Context context){
-	    final Intent alarmClockIntent = new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER);
+	    final Intent alarmClockIntent = new Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_LAUNCHER)
+    	.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
 	    // Verify clock implementation
 	    final String clockImpls[][] = {
@@ -25,18 +28,27 @@ public class ClockUtil {
 
 	    boolean foundClockImpl = false;
 
+	    final PackageManager packageManager = context.getPackageManager();
+
 	    for(int i=0; i<clockImpls.length; i++) {
 	        //final String vendor = clockImpls[i][0];
 	        final String packageName = clockImpls[i][1];
 	        final String className = clockImpls[i][2];
-
-            final ComponentName cn = new ComponentName(packageName, className);
-            alarmClockIntent.setComponent(cn);
-            foundClockImpl = true;
-
+	        try {
+	            final ComponentName cn = new ComponentName(packageName, className);
+	            packageManager.getActivityInfo(cn, PackageManager.GET_META_DATA);
+	            alarmClockIntent.setComponent(cn);
+	            //debug("Found " + vendor + " --> " + packageName + "/" + className);
+	            foundClockImpl = true;
+	            break;
+	        } catch (final NameNotFoundException e) {
+	        	// haven't found it yet
+	        }
 	    }
 
+
 	    if (foundClockImpl) {
+
 	        return PendingIntent.getActivity(context, 0, alarmClockIntent, 0);
 	    }else{
 	    	return null;

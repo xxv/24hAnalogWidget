fb7c98c1b9eba3d3a08eb961cd68aac910bacdc7
Steve Pomeroy <steve@staticfree.info>
2012-06-30 12:10:53 -0400

    Adds error handling for timezone / location inconsistencies
    Errors or no information for sunrise / sunset are now shown as a
    patterned background.

--- src/info/staticfree/android/twentyfourhour/SunPositionOverlay.java
+++ src/info/staticfree/android/twentyfourhour/SunPositionOverlay.java
@@ -1,17 +1,18 @@
 package info.staticfree.android.twentyfourhour;
+
 /*
- * Copyright (C) 2011 Steve Pomeroy <steve@staticfree.info>
- * 
+ * Copyright (C) 2011-2012 Steve Pomeroy <steve@staticfree.info>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,28 +26,40 @@ import uk.me.jstott.coordconv.LatitudeLongitude;
 import uk.me.jstott.sun.Sun;
 import uk.me.jstott.sun.Time;
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.Shader;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
+import android.util.Log;
 
 public class SunPositionOverlay implements DialOverlay {
+
+	private static final String TAG = SunPositionOverlay.class.getSimpleName();
 
 	private final LocationManager mLm;
 
 	private final RectF inset = new RectF();
 	private final LatitudeLongitude ll = new LatitudeLongitude(0,0);
 	private static Paint OVERLAY_PAINT = new Paint(Paint.ANTI_ALIAS_FLAG) ;
+	private static Paint OVERLAY_NO_INFO_PAINT = new Paint(Paint.ANTI_ALIAS_FLAG);
 
 	static {
 		OVERLAY_PAINT.setARGB(50, 0, 0, 0);
 		OVERLAY_PAINT.setStyle(Paint.Style.FILL);
+
 	}
 
 	public SunPositionOverlay(Context context){
 		mLm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+		OVERLAY_NO_INFO_PAINT.setShader(new BitmapShader(BitmapFactory.decodeResource(
+				context.getResources(), R.drawable.no_sunrise_sunset_tile), Shader.TileMode.REPEAT,
+				Shader.TileMode.REPEAT));
 	}
 
 	private Location getRecentLocation(){
@@ -80,6 +93,10 @@ public class SunPositionOverlay implements DialOverlay {
 		return (Analog24HClock.getHourHandAngle(h, m) + 270) % 360.0f;
 	}
 
+	private void drawPlaceholder(Canvas canvas) {
+		canvas.drawArc(inset, 0, 180, true, OVERLAY_NO_INFO_PAINT);
+	}
+
 	@Override
 	public void onDraw(Canvas canvas, int cX, int cY, int w, int h, Calendar calendar) {
 		final Location loc = getRecentLocation();
@@ -89,7 +106,7 @@ public class SunPositionOverlay implements DialOverlay {
 
 		if (loc == null){
 			// not much we can do if we don't have a location
-			canvas.drawArc(inset, 0, 180, true, OVERLAY_PAINT);
+			drawPlaceholder(canvas);
 			return;
 		}
 		ll.setLatitude(loc.getLatitude());
@@ -99,12 +116,21 @@ public class SunPositionOverlay implements DialOverlay {
 
 		final boolean dst = calendar.get(Calendar.DST_OFFSET) != 0;
 
-		final Time sunrise = Sun.sunriseTime(calendar, ll, tz, dst);
-		final float sunriseAngle = getHourArcAngle(sunrise.getHours(), sunrise.getMinutes());
+		try {
+			final Time sunrise = Sun.sunriseTime(calendar, ll, tz, dst);
+			final float sunriseAngle = getHourArcAngle(sunrise.getHours(), sunrise.getMinutes());
 
-		final Time sunset = Sun.sunsetTime(calendar, ll, tz, dst);
-		final float sunsetAngle = getHourArcAngle(sunset.getHours(), sunset.getMinutes());
+			final Time sunset = Sun.sunsetTime(calendar, ll, tz, dst);
+			final float sunsetAngle = getHourArcAngle(sunset.getHours(), sunset.getMinutes());
 
-		canvas.drawArc(inset, sunsetAngle, (360 + (sunriseAngle - sunsetAngle)) % 360, true, OVERLAY_PAINT);
+			canvas.drawArc(inset, sunsetAngle, (360 + (sunriseAngle - sunsetAngle)) % 360, true,
+					OVERLAY_PAINT);
+
+			// this can happen when lat/lon and the timezone are out of sync, causing impossible
+			// sunrise/sunset times to be calculated.
+		} catch (final IllegalArgumentException e) {
+			Log.e(TAG, "Error computing sunrise / sunset time", e);
+			drawPlaceholder(canvas);
+		}
 	}
 }

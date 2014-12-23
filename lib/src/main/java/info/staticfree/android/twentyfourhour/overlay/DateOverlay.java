7bf5f196ab8859698ed48b355a5675e210eedd34
Steve Pomeroy <steve@staticfree.info>
2014-12-23 08:08:01 -0500

    Updates to use released Android Wear watchface API
    * DateOverlay only shows tomorrow on month end

--- lib/src/main/java/info/staticfree/android/twentyfourhour/overlay/DateOverlay.java
+++ lib/src/main/java/info/staticfree/android/twentyfourhour/overlay/DateOverlay.java
@@ -9,68 +9,101 @@ import java.util.Calendar;
 import java.util.Locale;
 
 /**
- * An overlay that shows the date.
+ * An overlay that shows the date. Above the numeric date is the current month abbreviation.
+ * When tomorrow is a new month, this shows that too.
  */
 public class DateOverlay implements DialOverlay {
+    public static final float ROUNDED_RECT_RADIUS = 2f;
     private final float mOffsetY;
     private final float mOffsetX;
-    private float rectRatio = 1.61828f;
-    private float textSize = 20;
-    private float tomorrowScale = 0.5f;
-    private RectF bgrect = new RectF();
+    private static final float RECT_RATIO = 1.61828f;
+    private static final float TEXT_SIZE = 20;
+    private RectF tomorrowRect = new RectF();
     private RectF todayRect = new RectF();
     private Paint mBgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Paint mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint mTomorrowBgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint mTomorrowTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Paint mMonthTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private Calendar mTomorrow = Calendar.getInstance();
 
-    public DateOverlay(float offsetX, float offsetY) {
+    /**
+     * @param offsetX the x offset, in pixels, from the center.
+     * @param offsetY the y offset, in pixels, from the center.
+     */
+    public DateOverlay(final float offsetX, final float offsetY) {
         mOffsetX = offsetX;
         mOffsetY = offsetY;
         mBgPaint.setStyle(Paint.Style.FILL);
-        mTextPaint.setTextSize(textSize);
+        mBgPaint.setColor(Color.argb(255, 80, 80, 80));
+
+        mTextPaint.setTextSize(TEXT_SIZE);
         mTextPaint.setTextAlign(Paint.Align.CENTER);
+        mTextPaint.setColor(Color.argb(192, 255, 255, 255));
+
+        mTomorrowTextPaint.setTextSize(TEXT_SIZE);
+        mTomorrowTextPaint.setTextAlign(Paint.Align.CENTER);
+        mTomorrowTextPaint.setColor(Color.argb(96, 255, 255, 255));
+
+        mTomorrowBgPaint.setStyle(Paint.Style.FILL);
+        mTomorrowBgPaint.setColor(Color.argb(40, 255, 255, 255));
+
+        mMonthTextPaint.setTextAlign(Paint.Align.LEFT);
+        mMonthTextPaint.setTextSize(TEXT_SIZE * 0.6f);
+        mMonthTextPaint.setColor(Color.argb(127, 255, 255, 255));
     }
 
     @Override
-    public void onDraw(Canvas canvas, int cX, int cY, int w, int h, Calendar calendar, boolean sizeChanged) {
-        todayRect.set(cX + mOffsetX, cY + mOffsetY, cX + mOffsetX + textSize * rectRatio, cX + mOffsetY + textSize);
-
-        bgrect.set(todayRect);
-        bgrect.inset(3, 3);
-        bgrect.offset(todayRect.width() - 8, 0);
+    public void onDraw(Canvas canvas, int cX, int cY, int w, int h, Calendar calendar,
+                       boolean sizeChanged) {
+        todayRect.set(cX + mOffsetX, cY + mOffsetY, cX + mOffsetX + TEXT_SIZE * RECT_RATIO,
+                cX + mOffsetY + TEXT_SIZE);
         // Tomorrow
-        mTextPaint.setColor(Color.argb(96, 255, 255, 255));
-        mBgPaint.setColor(Color.argb(40, 255, 255, 255));
         mTomorrow.setTime(calendar.getTime());
-
         mTomorrow.add(Calendar.DAY_OF_MONTH, 1);
+
         boolean showNextMonth = mTomorrow.get(Calendar.MONTH) != calendar.get(Calendar.MONTH);
-        drawDay(canvas, mTomorrow, bgrect, mBgPaint, mTextPaint, showNextMonth);
+
+        // Under-draw tomorrow
+        if (showNextMonth) {
+            tomorrowRect.set(todayRect);
+            tomorrowRect.inset(3, 3);
+            tomorrowRect.offset(todayRect.width() - 8, 0);
+            drawDay(canvas, mTomorrow, tomorrowRect, mTomorrowBgPaint, mTomorrowTextPaint, true);
+        }
 
         // Main date.
-        mTextPaint.setColor(Color.argb(192, 255, 255, 255));
-        mBgPaint.setColor(Color.argb(255, 80, 80, 80));
-
         drawDay(canvas, calendar, todayRect, mBgPaint, mTextPaint, true);
     }
 
-    private void drawDay(Canvas canvas, Calendar when, RectF bg, Paint bgPaint, Paint textPaint, boolean showMonth) {
-        drawTextRectBg(canvas, String.valueOf(when.get(Calendar.DAY_OF_MONTH)), bg, bgPaint, textPaint);
-        textPaint.setTextAlign(Paint.Align.LEFT);
+    private void drawDay(final Canvas canvas, final Calendar when, final RectF bg,
+                         final Paint bgPaint, final Paint textPaint, final boolean showMonth) {
+        drawTextRectBg(canvas, String.valueOf(when.get(Calendar.DAY_OF_MONTH)), bg, bgPaint,
+                textPaint);
 
         if (showMonth) {
-            textPaint.setTextSize(bg.height() * 0.6f);
-            textPaint.setColor(Color.argb(127, 255, 255, 255));
-            canvas.drawText(when.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault()),
-                    bg.left + bg.width() * 0.05f, bg.top, textPaint);
+            canvas.drawText(
+                    when.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.getDefault()),
+                    bg.left + bg.width() * 0.05f, bg.top, mMonthTextPaint);
         }
 
     }
 
-    private void drawTextRectBg(Canvas canvas, CharSequence text, RectF location, Paint bgPaint, Paint textPaint) {
+    /**
+     * Draws the given text with a rounded rectangle background.
+     *
+     * @param canvas    the canvas to draw on.
+     * @param text      the text to draw.
+     * @param bgSize    the size and position of the rectangle.
+     * @param bgPaint   the background color.
+     * @param textPaint the text color and style; the text size will be adjusted.
+     */
+    private void drawTextRectBg(final Canvas canvas, final CharSequence text, final RectF bgSize,
+                                final Paint bgPaint, final Paint textPaint) {
         mTextPaint.setTextAlign(Paint.Align.CENTER);
-        canvas.drawRoundRect(location, 2f, 2f, bgPaint);
-        textPaint.setTextSize(location.height());
-        canvas.drawText(text, 0, text.length(), location.centerX(), location.bottom - location.height() * 0.15f, textPaint);
+        canvas.drawRoundRect(bgSize, ROUNDED_RECT_RADIUS, ROUNDED_RECT_RADIUS, bgPaint);
+        textPaint.setTextSize(bgSize.height());
+        canvas.drawText(text, 0, text.length(), bgSize.centerX(),
+                bgSize.bottom - bgSize.height() * 0.15f, textPaint);
     }
 }
--- wear/src/main/java/info/staticfree/android/twentyfourhour/WearFace.java
+++ wear/src/main/java/info/staticfree/android/twentyfourhour/WearFace.java
@@ -1,77 +1,181 @@
 package info.staticfree.android.twentyfourhour;
 
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
-import android.view.View;
+import android.os.Handler;
+import android.os.Message;
+import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
+import android.view.Gravity;
+import android.view.SurfaceHolder;
+
+import java.util.TimeZone;
 
 import info.staticfree.android.twentyfourhour.overlay.DateOverlay;
 import info.staticfree.android.twentyfourhour.overlay.HandsOverlay;
 import info.staticfree.android.twentyfourhour.overlay.SunPositionOverlay;
 import info.staticfree.android.twentyfourhour.wear.R;
 
-public class WearFace extends Activity {
-    private final static IntentFilter INTENT_FILTER;
+public class WearFace extends CanvasWatchFaceService {
 
-    static {
-        INTENT_FILTER = new IntentFilter();
-        INTENT_FILTER.addAction(Intent.ACTION_TIME_TICK);
-        INTENT_FILTER.addAction(Intent.ACTION_TIMEZONE_CHANGED);
-        INTENT_FILTER.addAction(Intent.ACTION_TIME_CHANGED);
-    }
-
-    private Analog24HClock mClock;
-    private BroadcastReceiver mTimeInfoReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context arg0, Intent intent) {
-            mClock.setTime(System.currentTimeMillis());
-        }
-    };
+    public static final float SUN_POSITION_OVERLAY_SCALE = 0.61345f;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wear_face);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+    public Engine onCreateEngine() {
+        return new Engine();
+    }
 
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+    private class Engine extends CanvasWatchFaceService.Engine {
+        private static final int MSG_UPDATE_TIME = 100;
+        private static final long INTERACTIVE_UPDATE_RATE_MS = 60 * 1000;
+        final Handler mUpdateTimeHandler = new Handler() {
             @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-
-                initializeClock(stub);
+            public void handleMessage(Message message) {
+                switch (message.what) {
+                    case MSG_UPDATE_TIME:
+                        invalidate();
+                        if (shouldTimerBeRunning()) {
+                            long timeMs = System.currentTimeMillis();
+                            long delayMs = INTERACTIVE_UPDATE_RATE_MS -
+                                    (timeMs % INTERACTIVE_UPDATE_RATE_MS);
+                            mUpdateTimeHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIME, delayMs);
+                        }
+                        break;
+                }
             }
-        });
-    }
+        };
+        /* receiver to update the time zone */
+        final BroadcastReceiver mTimeZoneReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent.hasExtra("time-zone")) {
+                    mClock.setTimezone(TimeZone.getTimeZone(intent.getStringExtra("time-zone")));
+                }
+            }
+        };
+        private boolean mViewSizeInvalid = true;
+        private Analog24HClock mClock;
+        private boolean mRegisteredTimeZoneReceiver;
 
+        private void updateTimer() {
+            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME);
 
-    private void initializeClock(View stub) {
-        mClock = (Analog24HClock) stub.findViewById(R.id.face);
-        mTimeInfoReceiver.onReceive(WearFace.this, registerReceiver(null, INTENT_FILTER));    //  Here, we're just calling our onReceive() so it can set the current time.
-        registerReceiver(mTimeInfoReceiver, INTENT_FILTER);
-        SunPositionOverlay sunPositionOverlay = new SunPositionOverlay(getApplicationContext());
-        Location test = new Location("foo");
-        test.setLatitude(42.4);
-        test.setLongitude(-71.1);
-        sunPositionOverlay.setLocation(test);
-        sunPositionOverlay.setScale(0.61345f);
-        sunPositionOverlay.setShadeAlpha(60);
-        mClock.addDialOverlay(sunPositionOverlay);
+            if (shouldTimerBeRunning()) {
+                mUpdateTimeHandler.sendEmptyMessage(MSG_UPDATE_TIME);
+            }
+        }
 
-        DateOverlay dateOverlay = new DateOverlay(30,-30);
-        mClock.addDialOverlay(dateOverlay);
-        mClock.setHandsOverlay(new HandsOverlay(getApplicationContext(),
-                R.drawable.round_hour_hand, R.drawable.round_minute_hand));
-        mClock.setFace(R.drawable.round_clock_face);
-    }
+        private boolean shouldTimerBeRunning() {
+            return isVisible() && !isInAmbientMode();
+        }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(mTimeInfoReceiver);
+        private void initializeClock() {
+            mClock = new Analog24HClock(getApplicationContext());
+            SunPositionOverlay sunPositionOverlay = new SunPositionOverlay(getApplicationContext());
+            Location test = new Location("manual");
+            test.setLatitude(42.4);
+            test.setLongitude(-71.1);
+            sunPositionOverlay.setLocation(test);
+            sunPositionOverlay.setScale(SUN_POSITION_OVERLAY_SCALE);
+            sunPositionOverlay.setShadeAlpha(60);
+            mClock.addDialOverlay(sunPositionOverlay);
+
+            DateOverlay dateOverlay = new DateOverlay(30, -30);
+            mClock.addDialOverlay(dateOverlay);
+            mClock.setHandsOverlay(
+                    new HandsOverlay(getApplicationContext(), R.drawable.round_hour_hand,
+                            R.drawable.round_minute_hand));
+            mClock.setFace(R.drawable.round_clock_face);
+        }
+
+        @Override
+        public void onCreate(SurfaceHolder holder) {
+            initializeClock();
+
+               /* configure the system UI */
+            setWatchFaceStyle(new WatchFaceStyle.Builder(WearFace.this)
+                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_SHORT)
+                    .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
+                    .setShowSystemUiTime(false).setHotwordIndicatorGravity(Gravity.CENTER)
+                    .setStatusBarGravity(Gravity.CENTER)
+                    .setViewProtection(WatchFaceStyle.PROTECT_STATUS_BAR)
+                    .setPeekOpacityMode(WatchFaceStyle.PEEK_OPACITY_MODE_TRANSLUCENT).build());
+            setTouchEventsEnabled(false);
+
+            updateTimer();
+        }
+
+        @Override
+        public void onPropertiesChanged(Bundle properties) {
+            /* get device features (burn-in, low-bit ambient) */
+        }
+
+        @Override
+        public void onTimeTick() {
+            invalidate();
+        }
+
+        @Override
+        public void onAmbientModeChanged(boolean inAmbientMode) {
+            /* the wearable switched between modes */
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            mClock.setTime(System.currentTimeMillis());
+
+            if (mViewSizeInvalid) {
+                mClock.measure(bounds.width(), bounds.height());
+                mClock.layout(bounds.left, bounds.top, bounds.right, bounds.bottom);
+                mViewSizeInvalid = false;
+            }
+
+            mClock.draw(canvas);
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+
+            if (visible) {
+                registerTimeZoneReceiver();
+
+                // Update time zone in case it changed while we weren't visible.
+                mClock.setTimezone(TimeZone.getDefault());
+            } else {
+                unregisterTimeZoneReceiver();
+            }
+
+            // Whether the timer should be running depends on whether we're visible and
+            // whether we're in ambient mode), so we may need to start or stop the timer.
+            updateTimer();
+        }
+
+        private void registerTimeZoneReceiver() {
+            if (mRegisteredTimeZoneReceiver) {
+                return;
+            }
+
+            mRegisteredTimeZoneReceiver = true;
+            final IntentFilter filter = new IntentFilter();
+            filter.addAction(Intent.ACTION_TIME_CHANGED);
+            filter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+
+            registerReceiver(mTimeZoneReceiver, filter);
+        }
+
+        private void unregisterTimeZoneReceiver() {
+            if (!mRegisteredTimeZoneReceiver) {
+                return;
+            }
+            mRegisteredTimeZoneReceiver = false;
+            unregisterReceiver(mTimeZoneReceiver);
+        }
     }
 }
